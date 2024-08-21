6562d0771a70c205776500140c3ebe2c35c28638
JohnBaska <131826014+JohnBaska@users.noreply.github.com>
2024-08-20 23:23:59 -0300

    new: new project

---
+++ Principal.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/experience')
+def experience():
+    return render_template('experience.html')
+
+@app.route('/education')
+def education():
+    return render_template('education.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
